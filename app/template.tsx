762e627fe704fe1df6b8c86f5ae0620e87deccf5
cms459800-wq <cms459800@gmail.com>
2026-09-11 12:18:40 +0900

    Load icon styling for all page families

--- app/template.tsx
+++ app/template.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./icon-system.css";
+import "./icon-system-pages.css";
 
 export default function Template({ children }: { children: ReactNode }) {
   return children;
